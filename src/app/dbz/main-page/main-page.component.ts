a0ef0d62d6a045b8a79cc205cc8523ae4de99d7d
Aaron Rmz <aaronrmz@MacBook-Pro.local>
2022-03-10 16:27:42 -0600

    personajes y add personaje

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, EventEmitter } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    {nombre: 'Goku', poder: 15000}, 
+    {nombre: 'Vegeta', poder: 7500}
+  ];
+
+  nuevo: Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1000
+  }
+
+  agregarNuevoPersonaje(personaje: Personaje){
+    this.personajes.push(personaje);
+  }
+
+}
